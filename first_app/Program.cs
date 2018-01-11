aaae53fc7a3e994f07c2a1024a02fd719d228a29
Venkat Dommalapati <dvenkateswarlu@hotmail.com>
2018-01-10 21:24:17 -0800

    Adding initial files, added comments to readme.md

---
+++ first_app/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace first_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Using Console.WriteLine, you can output any string to the terminal/console");
+            
+        }
+    }
+}
